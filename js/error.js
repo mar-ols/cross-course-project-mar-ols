da19f7f75d4992e61d632649ff9bf41c1c86c3f3
Marte <125874444+mar-ols@users.noreply.github.com>
2023-09-09 15:40:38 +0200

    Updated error message

--- js/error.js
+++ js/error.js
@@ -2,7 +2,7 @@ const errorHtml = `<div class="error">
                         <p><img src="../images/assets/icons/wizard.png" alt="Icon of a wizard." class="wizard">
                         You shall not pass!
                         <img src="../images/assets/icons/wizard.png" alt="Icon of a wizard." class="wizard"></p>
-                        <p>Just kidding. Something went wrong, the wizards will look into it,  posthaste!</p>
+                        <p>Just kidding. Something's gone wrong on our end, the wizards will look into it,  posthaste!</p>
                      </div>`;
 
 export function error() {
